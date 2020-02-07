4923e07921f72508b7953cb85e4d098794963450
Khalilurrehman <khalil_rehman1993@ymail.com>
2020-02-08 01:51:01 +0400

    Updates

---
+++ precache-manifest.fa2db9ad04be387b51cdc8f153dfd0ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0a83f8d7c89cab9361a727af4a45f5",
+    "url": "/Abbc-Test/index.html"
+  },
+  {
+    "revision": "2b4153018c997e73ae2b",
+    "url": "/Abbc-Test/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "6897d32f024bd3eb8898",
+    "url": "/Abbc-Test/static/css/main.b42cddb7.chunk.css"
+  },
+  {
+    "revision": "2b4153018c997e73ae2b",
+    "url": "/Abbc-Test/static/js/2.cb42217b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Abbc-Test/static/js/2.cb42217b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6897d32f024bd3eb8898",
+    "url": "/Abbc-Test/static/js/main.e8727c4f.chunk.js"
+  },
+  {
+    "revision": "b289d8aad35560de93f6",
+    "url": "/Abbc-Test/static/js/runtime-main.4c0f348a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Abbc-Test/static/media/logo.5d5d9eef.svg"
+  }
+]);
